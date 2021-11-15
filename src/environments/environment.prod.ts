83a96270df70248fcf9b11f7b6e8fae0f03b01fe
IreneMars <irenecharm97@gmail.com>
2021-11-15 15:55:39 +0100

    some fixes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,17 @@
+import { DeviatedValueParams, FricError } from "src/app/models/fricError.model";
+
+const average = [
+  'mean',
+  'median',
+  'mode'
+];
+
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'http://localhost:3000/api',
+  SOCKET_ENDPOINT: 'http://localhost:3000',
+  errors: [
+    new FricError('duplicate-row', null),
+    new FricError('deviated-value', new DeviatedValueParams('', average, 0))
+  ]
 };
